1bee6ed6965c387db33315140ebd9ba56555fede
George <georgekorob@gmail.com>
2022-02-05 13:47:21 +0300

    router urls

--- drftodo/urls.py
+++ drftodo/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from authors.views import AuthorModelViewSet
+
+router = DefaultRouter()
+router.register('authors', AuthorModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/', include(router.urls))
 ]
