efd734c16d93a6320905e56fe45d8ac019e97b70
George <georgekorob@gmail.com>
2022-02-05 13:47:11 +0300

    views

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Author
+from .serializers import AuthorModelSerializer
+
 
 # Create your views here.
+class AuthorModelViewSet(ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorModelSerializer
