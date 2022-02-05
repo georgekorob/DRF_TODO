27ee70800a74c50322a0d328b1f535f8b926e0e9
George <georgekorob@gmail.com>
2022-02-05 14:31:28 +0300

    fill_db and superuser

---
+++ authapp/management/commands/fill_db.py
@@ -0,0 +1,34 @@
+import json
+import os
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+from authapp.models import User
+from authors.models import Author
+
+
+def load_from_json(file_name):
+    try:
+        with open(file_name, mode='r', encoding='utf-8') as infile:
+            return json.load(infile)
+    except UnicodeDecodeError:
+        with open(file_name, mode='r', encoding='windows-1251') as infile:
+            return json.load(infile)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        User.objects.all().delete()
+        User.objects.create_superuser(username=os.getenv('SUPER_USER_USERNAME'),
+                                      password=os.getenv('SUPER_USER_PASSWORD'),
+                                      email=os.getenv('SUPER_USER_EMAIL'))
+        User.objects.create_superuser(username='george',
+                                      password='testpass',
+                                      email='george@test.ru',
+                                      first_name='george',
+                                      last_name='korobanov')
+
+        # User.objects.all().delete()
+        # Author.objects.all().delete()
+        # for name in ['authapp', 'authors']:
+        #     filename = f'./{name}/fixtures/{name}.json'
+        #     call_command('loaddata', filename, app_label=name)
