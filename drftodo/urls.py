6880bb5bfc38491e86fda3b1925a0888f3bcf57e
George <georgekorob@gmail.com>
2022-02-05 14:14:20 +0300

    add user api

--- drftodo/urls.py
+++ drftodo/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from authapp.views import UserModelViewSet
 from authors.views import AuthorModelViewSet
 
 router = DefaultRouter()
 router.register('authors', AuthorModelViewSet)
+router.register('users', UserModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
