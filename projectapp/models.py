c37b6d0963d965dc57bd1c200cb8de91780f8be3
George <georgekorob@gmail.com>
2022-02-10 15:41:44 +0300

    model todo

--- projectapp/models.py
+++ projectapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+
 from authapp.models import User
 
 
@@ -13,4 +15,18 @@ class Project(models.Model):
 
 
 class Todo(models.Model):
-    pass
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    text = models.TextField(null=True, blank=True)
+    create_date = models.DateField(editable=False)
+    update_date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.project} {self.text[:10]} {self.update_date} {self.user}'
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.create_date = timezone.now()
+        self.update_date = timezone.now()
+        return super().save(*args, **kwargs)
