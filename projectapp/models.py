5f796834043272b19cd543aeab6514564fb01314
George <georgekorob@gmail.com>
2022-02-10 15:22:39 +0300

    model project

--- projectapp/models.py
+++ projectapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from authapp.models import User
+
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=64)
+    link = models.URLField()
+    users = models.ManyToManyField(User)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Todo(models.Model):
+    pass
