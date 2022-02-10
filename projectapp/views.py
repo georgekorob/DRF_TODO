f21c9838606a60d05a75d65f320682e1b3596cf0
George <georgekorob@gmail.com>
2022-02-10 15:47:45 +0300

    views

--- projectapp/views.py
+++ projectapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from projectapp.models import Project, Todo
+from .serializers import ProjectModelSerializer, TodoModelSerializer
+
 
 # Create your views here.
+class ProjectModelViewSet(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectModelSerializer
+
+
+class TodoModelViewSet(ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoModelSerializer
