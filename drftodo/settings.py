4fb5ce61f0e17bd4a3da8daa4aff36e27cec8d09
George <georgekorob@gmail.com>
2022-02-14 10:00:00 +0300

    PAGE_SIZE

--- drftodo/settings.py
+++ drftodo/settings.py
@@ -158,4 +158,7 @@ REST_FRAMEWORK = {
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
         # Any other parsers
     ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
 }
