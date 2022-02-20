bab0f71a6750b850b819fbd102f3c8a29b7de8e9
George <georgekorob@gmail.com>
2022-02-20 17:45:46 +0300

    BasicAuthentication

--- drftodo/settings.py
+++ drftodo/settings.py
@@ -162,4 +162,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 100,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+    ]
 }
