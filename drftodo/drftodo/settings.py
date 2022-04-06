22e4b8bc181021ad3f3428a1ff16a90d3a772409
George <georgekorob@gmail.com>
2022-04-06 16:32:50 +0300

    move frontend

--- drftodo/drftodo/settings.py
+++ drftodo/drftodo/settings.py
@@ -75,8 +75,8 @@ ROOT_URLCONF = 'drftodo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        # 'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / '../frontend/build'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -140,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (BASE_DIR / '../frontend/build/static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
