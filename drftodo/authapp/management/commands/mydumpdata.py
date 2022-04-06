097b77f6f4408ad0b3d6cce13f5052714655ea24
George <georgekorob@gmail.com>
2022-04-06 19:33:19 +0300

    docker

--- drftodo/authapp/management/commands/mydumpdata.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from django.core.management import call_command
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        for name in ['authapp']:
-            with open(f'./{name}/fixtures/{name}.json', 'w') as file:
-                call_command('dumpdata', name, format='json', indent=3, stdout=file)
--- drftodo/drftodo/settings.py
+++ drftodo/drftodo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import platform
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -95,13 +96,24 @@ WSGI_APPLICATION = 'drftodo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if platform.system() == 'Windows':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'drftodo_db',
+            'USER': 'geekbrains',
+            'PASSWORD': '1',
+            'HOST': 'db',
+            'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
